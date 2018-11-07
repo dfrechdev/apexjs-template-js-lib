23e478bb100ce3ba62eacd5be0d90af4d65497f9
etops77 <etops77@gmail.com>
2018-11-07 21:39:24 +0100

    changed export

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-module.exports = function setupLibrary(library) {
+const inquirer = require('inquirer');
+
+module.exports = { setupLibrary };
+
+function setupLibrary(library, logger) {
     logger.logInfo('Please answer the following questions:');
-    return inquirer.prompt(this.questions).then((answers) => {
+    return inquirer.prompt(getQuestions).then((answers) => {
         logger.log('Thanks\n');
         return new Promise((resolve, reject) => {
             library.code = answers['library-code'];
@@ -19,4 +23,29 @@ function writeLibrayDetails(library) {
     packageJSON.version = library.version;
     packageJSON.libraryCode = library.code;
     fs.writeFileSync(`${library.getLibraryPath()}/package.json`, JSON.stringify(packageJSON, null, 4));
+}
+
+function getQuestions() {
+    return [
+        {
+            name: 'library-code',
+            type: 'input',
+            default: this.name,
+            message: 'Library code:',
+            validate: function(input) {
+                if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+                else return 'The library code may only include letters, numbers, underscores and hashes.';
+            }
+        },
+        {
+            name: 'initial-version',
+            type: 'input',
+            message: 'Initial version:',
+            default: '1.0.0',
+            validate: function(input) {
+                if (semverRegex().test(input)) return true;
+                else return 'The initial version must match a semantic versions such as 0.0.1';
+            }
+        }
+    ];
 }
