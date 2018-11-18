a2c2df2fc899db98284d3668c32f9733e6fbc02e
etops77 <etops77@gmail.com>
2018-11-18 15:57:49 +0100

    refactored library refs to app refs

--- index.js
+++ index.js
@@ -4,35 +4,37 @@ const inquirer = require('inquirer');
 
 module.exports = { setupLibrary };
 
-function setupLibrary(library, logger) {
-    logger.logInfo('Please answer the following questions:');
-    return inquirer.prompt(getQuestions(library)).then((answers) => {
-        logger.log('Thanks\n');
+function setupApp(appName, appPath) {
+    console.log('Please answer the following questions:');
+    return inquirer.prompt(getQuestions(appName)).then((answers) => {
         return new Promise((resolve, reject) => {
-            library.code = answers['library-code'];
-            library.version = answers['initial-version'];
-
-            writeLibrayDetails(library);
+            const libDetails = {
+                name = appName,
+                path = appPath,
+                code = answers['library-code'],
+                version = answers['initial-version']
+            };
+            writeLibDetails(app);
             resolve('done');
         });
     });
 }
 
-function writeLibrayDetails(library) {
-    const packageJsonPath = `${library.getLibraryPath()}/package.json`;
+function writeLibDetails(libDetails) {
+    const packageJsonPath = `${libDetails.path}/package.json`;
     let packageJSON = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-    packageJSON.name = library.name;
-    packageJSON.version = library.version;
-    packageJSON.libraryCode = library.code;
-    fs.writeFileSync(`${library.getLibraryPath()}/package.json`, JSON.stringify(packageJSON, null, 4));
+    packageJSON.name = libDetails.name;
+    packageJSON.version = libDetails.version;
+    packageJSON.libraryCode = libDetails.code;
+    fs.writeFileSync(`${libDetails.path}/package.json`, JSON.stringify(packageJSON, null, 4));
 }
 
-function getQuestions(library) {
+function getQuestions(appName) {
     return [
         {
             name: 'library-code',
             type: 'input',
-            default: library.name,
+            default: appName,
             message: 'Library code:',
             validate: function(input) {
                 if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
