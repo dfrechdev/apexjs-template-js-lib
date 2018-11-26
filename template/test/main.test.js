f329d8d101140c5211e2879ddee5e3bd60a5c077
Daniel Frech <dfrechdev@gmail.com>
2018-11-26 14:45:44 +0100

    #3: added setup tests

---
+++ template/test/main.test.js
@@ -0,0 +1,24 @@
+import test from 'ava';
+import { projectInfo, setupTests } from '../src/main';
+
+test('validate project info of library', t => {
+    t.truthy(projectInfo);
+    t.is(projectInfo.name, 'NPM_PACKAGE_PROJECT_NAME');
+    t.is(projectInfo.version, 'NPM_PACKAGE_PROJECT_VERSION');
+});
+
+test('test es6 promises', t => {
+    return setupTests.promiseTest().then(result => {
+        t.pass(result);
+    });
+});
+
+test('test es6 Array.includes()', t => {
+    t.true(setupTests.arrayIncludesTest(['a', 'b', 'c', 'd'], 'b'));
+    t.false(setupTests.arrayIncludesTest(['a', 'b', 'c', 'd'], 'w'));
+});
+
+test('test ecma2017 async/await', async t => {
+    const value = await setupTests.asyncAwaitTest();
+    t.is(value, 200);
+});
